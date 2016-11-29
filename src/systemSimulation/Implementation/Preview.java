7b29dba96aa43df9fa89bad952e5fd6876f950b5
MessasKouseila <k_messas@yahoo.fr>
2016-11-29 02:36:58 +0100

    ajout des diagramms

--- src/systemSimulation/Implementation/Preview.java
+++ src/systemSimulation/Implementation/Preview.java
@@ -1,27 +1,25 @@
 package systemSimulation.Implementation;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.File;
 
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import systemAscenseur.Interface.ObserverNiveau;
 import systemAscenseur.Interface.ObserverSurcharge;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.SwingConstants;
-import java.awt.Font;
 
 public class Preview extends JFrame implements ObserverSurcharge,ObserverNiveau{
 	/*
